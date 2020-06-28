c8fa1ae313604955e7c7db53f29df9c2420f9c52
Fledson Henrique <Fledsonhenrique@gmail.com>
2020-06-28 03:41:17 -0300

    atualizando

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,7 +2,6 @@ const botaoPesquisa = document.querySelector("#page-home main a") //Buscando no 
 const modal = document.querySelector("#modal") //Buscando pelo modal dentro do index
 const fechar = document.querySelector("#modal .header a")
 
-
 // adicionando um ouvidor de eventos no botão, para mostrar a tela de pesquisa de pontos de coleta
 botaoPesquisa.addEventListener("click", () => {
     modal.classList.remove("hide") //acessando a classe list e removendo a classe "HIDE" para que a tela apareça quando o botão for clicado
